34dc6795e2073d6526e9e6e0e02481b2fade71ca
Konstantin Gusev <kostinus@gmail.com>
2020-06-17 15:25:11 +0300

    *

---
+++ gusev_age.py
@@ -0,0 +1,18 @@
+kinder = range(0,6)
+school = range(6,18)
+university = range(18,24)
+age = input("Сколько тебе лет, дорогой пользователь?\n")
+try:
+  age=float(age)
+  if age in kinder:
+    print("Тебе пора в детский сад.")
+  elif age in school:
+    print("Тебе пора в школу.")
+  elif age in university:
+    print("Тебе пора в институт.")
+  else:
+    print("Тебе пора на работу.")
+except ValueError:
+  print("Такого не бывает.")
+#else:
+ # print("Такого возраста не бывает.")
